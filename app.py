9a5521412ebbbc469b2c318969dd36c18eec68d9
Charanvir <charanvir123@gmail.com>
2023-03-17 16:45:45 -0400

    connection made to MySQL and flask backend connected to database

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+print(os.getenv("PASSWORD"))
 # Create an app instance
 app = Flask(__name__)
+# This will guard the appliction from hackers, cookies, and hijacking functionality
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+# Specify which database we are using
+app.config[
+    "SQLALCHEMY_DATABASE_URI"] = f"mysql://{os.getenv('USERNAME')}:{os.getenv('DB_PASSWORD')}@localhost/{os.getenv('DATABASE')}"
+db = SQLAlchemy(app)
+
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
 
 
 # Requests (HTTP requests)
@@ -13,14 +33,17 @@ def index():
         email = request.form["email"]
         date = request.form["date"]
         occupation = request.form["occupation"]
-        print(first_name)
-        print(last_name)
-        print(email)
-        print(date)
-        print(occupation)
+
+        form = Form(first_name=first_name, last_name=last_name, email=email, date=date, occupation=occupation)
+
+        db.session.add(form)
+        db.session.commit()
 
     return render_template("index.html")
 
 
-# Run the flask app
-app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        # Run the flask app
+        app.run(debug=True, port=5001)
