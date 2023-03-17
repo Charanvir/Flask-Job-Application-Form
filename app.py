cc42cf143a5c0dabf04d8ba6b00a17971ce45b20
Charanvir <charanvir123@gmail.com>
2023-03-17 16:03:57 -0400

    implemented accessing the form values with the flask backend using a post method

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Create an app instance
 app = Flask(__name__)
 
+
 # Requests (HTTP requests)
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+        print(first_name)
+        print(last_name)
+        print(email)
+        print(date)
+        print(occupation)
+
     return render_template("index.html")
 
 
